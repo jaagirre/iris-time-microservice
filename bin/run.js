9d3df0cb3daa997c4ec298aa979ee8f0f4d933af
joseba a. agirre <jaagirre@mondragon.edu>
2018-01-25 14:59:16 +0100

    Cambios para dapatra a cloud foundry

--- bin/run.js
+++ bin/run.js
@@ -1,4 +1,7 @@
 'use strict';
+//para cargar el fichero .env en process.env
+require('dotenv').load();
+const config = require('../config/index');
 
 const request = require('superagent');
 const service = require('../server/service');
@@ -13,7 +16,8 @@ server.on('listening', function(){
     console.log(`IRIS-time is listening on ${server.address().port} in ${service.get('env')} mode`);
 
     const announce = ()=>{
-        request.put(`http://127.0.0.1:3000/service/time/${server.address().port}`, (err, res)=>{
+        //request.put(`http://127.0.0.1:3000/service/time/${server.address().port}`, (err, res)=>{
+        request.put(`${config.iris_service_regfistry_url}${server.address().port}`, (err, res)=>{
             if (err){
                 console.log(err);
                 console.log('Error connecting to Iris');
